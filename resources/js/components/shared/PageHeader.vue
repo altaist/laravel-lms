358050343a6a5392533e11e640a02fea660d55d5
Altaist <apechersky@ya.ru>
2025-01-26 23:10:07 +0700

    fix header

--- resources/js/components/shared/PageHeader.vue
+++ resources/js/components/shared/PageHeader.vue
@@ -1,16 +1,23 @@
 <template>
     <div class="q-pa-md sticky top bg-white border" style="z-index: 1000;">
-        <div class="row items-center">
-            <div class="col" v-if="leftBtnIcon">
+        <div class="row items-center justify-between">
+            <div class="col-2" v-if="leftBtnIcon">
                 <btn :icon="leftBtnIcon" @click="onLeftBtnlick"/>
             </div>
-            <div class="col text-center"><page-title>{{title}}</page-title></div>
-            <div class="col text-right" v-if="rightBtnIcon"><btn :icon="rightBtnIcon" @click="onRightBtnClick"/></div>
+            <div class="col-2" v-else></div>
+            
+            <div class="col-8 text-center">
+                <page-title>{{title}}</page-title>
+            </div>
+            
+            <div class="col-2 text-right">
+                <btn v-if="rightBtnIcon" :icon="rightBtnIcon" @click="onRightBtnClick"/>
+            </div>
         </div>
     </div>
 </template>
-<script setup>
 
+<script setup>
 const props = defineProps({
     title: {
         type: String
@@ -19,8 +26,6 @@ const props = defineProps({
         type: String,
         default: 'primary'
     },
-
-
     leftBtnIcon: {
         type: String,
     },
@@ -31,15 +36,12 @@ const props = defineProps({
         type: Boolean,
         default: true
     },
-
-
     rightBtnIcon: {
         type: String
     },
     rightBtnRoute: {
         type: String
     }
-
 });
 
 const emit = defineEmits(['click:left', 'click:right', 'click:title']);
@@ -54,12 +56,10 @@ const onLeftBtnlick = () => {
     return emit("click:left")
 }
 
-
 const onRightBtnClick = () => {
     if(props.rightBtnRoute) {
         return redirect(props.rightBtnRoute);
     }
     return emit("click:right")
 }
-
 </script>
