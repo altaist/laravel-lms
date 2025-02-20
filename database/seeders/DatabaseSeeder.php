73944abadf3b67c11a5fbe5828fb58914c3cba58
Altaist <apechersky@ya.ru>
2025-02-20 15:37:26 +0700

    add autoimport on seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
@@ -30,6 +31,33 @@ class DatabaseSeeder extends Seeder
         // Проверяем, нужно ли сидировать тестовые данные
         if ($this->command->confirm('Хотите добавить тестовые данные?', false)) {
             $this->call(TestSeeder::class);
+        }
+
+        // Спрашиваем про импорт пользователей
+        if ($this->command->confirm('Хотите импортировать пользователей из import.csv?', false)) {
+            $this->command->info('Начинаем импорт пользователей...');
+            
+            try {
+                Artisan::call('app:import-users', [
+                    '--clear' => true,
+                    '--file' => 'import.csv'
+                ], $this->command->getOutput());
+                
+                $this->command->info('Импорт пользователей завершен');
+
+                // После успешного импорта пользователей спрашиваем про платежи
+                if ($this->command->confirm('Хотите импортировать платежи из payments.csv?', false)) {
+                    $this->command->info('Начинаем импорт платежей...');
+                    
+                    Artisan::call('payments:import', [
+                        'file' => 'payments.csv'
+                    ], $this->command->getOutput());
+                    
+                    $this->command->info('Импорт платежей завершен');
+                }
+            } catch (\Exception $e) {
+                $this->command->error('Ошибка при импорте: ' . $e->getMessage());
+            }
         }
     }
 }
